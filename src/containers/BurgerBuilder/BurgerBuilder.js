e32f4c24f2ba2b3158246196de059879bdac8800
Matt Smithson <matt.s@modula.co>
2019-02-17 17:38:54 +0000

    handle multiple ingredients dynamically and when there are no ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
+// Components
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    };
 
     render() {
         return (
             <>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build controls</div>
             </>
         );
