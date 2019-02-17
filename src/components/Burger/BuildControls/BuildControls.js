0f93387fc8dc0f4830aa61a2f74d8ecf311dd5d2
Matt Smithson <matt.s@modula.co>
2019-02-17 19:30:22 +0000

    add and remove ingredients and display updated price

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// Components
+import BuildControl from './BuildControl/BuildControl';
+
+// Styles
+import classes from './BuildControls.module.css';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = props => (
+    <div className={classes.BuildControls}>
+        <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(control => (
+            <BuildControl added={() => props.ingredientAdded(control.type)}
+                          removed={() => props.ingredientRemoved(control.type)}
+                          key={control.label}
+                          label={control.label}
+                          disabled={props.disabled[control.type]} />
+        ))}
+    </div>
+);
+
+export default buildControls;
