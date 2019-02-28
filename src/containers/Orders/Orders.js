742680763dc5e3a9421c4415749ab7b270d91996
Matt Smithson <matt.s@modula.co>
2019-02-28 16:17:54 +0000

    finish orders page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 
+// Axios instance
+import axios from '../../axios-orders';
+
 // Components
 import Order from '../../components/Order/Order';
+import withError from '../../hoc/withError/withError';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        id: key,
+                        ...res.data[key]
+                    })
+                }
+                this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            });
+    }
 
     render() {
         return (
             <div>
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order key={order.id}
+                           ingredients={order.ingredients}
+                           price={+order.price} />
+                ))}
             </div>
         );
     }
 
 }
 
-export default Orders;
+export default withError(Orders, axios);
